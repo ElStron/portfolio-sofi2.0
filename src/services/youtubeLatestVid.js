e1b18dc74774e24eb37613755ffc0241aeb731d2
Stron <luisstron45@gmail.com>
2024-09-27 23:24:59 -0600

    Update youtubeLatestVid.js

--- src/services/youtubeLatestVid.js
+++ src/services/youtubeLatestVid.js
@@ -8,17 +8,18 @@ export const fetchYouTubeVideos = async (channelId) => {
   try {
     const response = await fetch(reqURL);
     if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
+      console.log("s")
+      //throw new Error(`HTTP error! status: ${response.status}`);
     }
 
     const result = await response.json();
     if (result.items && result.items.length > 0) {
       return result.items;
     } else {
-      throw new Error("No videos found in the feed.");
+      //throw new Error("No videos found in the feed.");
     }
   } catch (error) {
     console.error("Error fetching videos:", error);
-    throw error;
+    //throw error;
   }
 };
