5248e3865dfdc5d8c652ce681123da0b94a4b68e
Stron <luisstron45@gmail.com>
2024-12-31 17:51:20 -0600

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,2 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-
-
-// interface ImportMetaEnv {
-//     readonly YT_API: string;
-//     readonly WPGRAPHQL_URL: string;
-//     readonly FORBIDDEN_WORDS: string;
-//     readonly WPGRAPHQL_URL: string;
-// }
